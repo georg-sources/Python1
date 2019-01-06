323b4d6a2bc992ee795dce22424bbd7e5196cf07
georg <georg@WIN10>
2019-01-06 12:51:08 +0100

    Parameterübergabe2

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,4 +1,10 @@
 
-def Hallo(Beruessung):
+def Hallo(Count, *pars):
     #Dies ist eine Funktion
-    print("Dies ist eine Funktion")
+    #print("Anzahl der Parameter ist " + str(Count))
+    for par in pars:
+        print(par)
+    return Count
+
+#Hallo(2,"Hallo","Test")
+print(Hallo(2,"Hallo","Test"))
